8f07a08b096a4b76072c4334e537fbd74c65f56e
Killoconq <fjc1997v@gmail.com>
2026-04-20 13:22:45 -0600

    cambios

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -1,5 +1,10 @@
 return {
   'windwp/nvim-ts-autotag',
   event = { 'BufReadPre', 'BufNewFile' },
-  opts = {},
+  opts = {
+    opts = {
+      -- Avoid InsertLeave crashes in prompt/non-parser buffers (e.g. Telescope)
+      enable_rename = false,
+    },
+  },
 }
